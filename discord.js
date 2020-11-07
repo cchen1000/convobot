c83b3fc8eb3e7dbbe88267409d43b68c07f1394b
Henry <henryqi301@gmail.com>
2020-11-07 15:58:33 -0500

    added Bot connection to channel and a stream connection

--- discord.js
+++ discord.js
@@ -5,7 +5,7 @@ client.once('ready', () => {
     console.log('Ready!');
 });
 
-client.login('token')
+client.login("Nzc0NzI1OTEyMzYwMjU1NTQw.X6b9uw.s8a-o8NabXQTNwhgx-5BIxkfmGM")
 
 client.on('message', message => {
     console.log(message.content);
@@ -14,3 +14,20 @@ client.on('message', message => {
         message.channel.send('echo');
     }
 });
+
+client.on('message', async message => {
+    // Voice only works in guilds, if the message does not come from a guild,
+    // we ignore it
+    if (!message.guild) return;
+  
+    if (message.content === '!join') {
+      // Only try to join the sender's voice channel if they are in one themselves
+      if (message.member.voice.channel) {
+        const connection = await message.member.voice.channel.join();
+      } else {
+        message.reply('You need to join a voice channel first!');
+      }
+      //This is going to be the stream that can go with Google stuff?
+      const ReadableStream = connection.voiceReceiver.createStream(message.member.user);
+    }
+  });
