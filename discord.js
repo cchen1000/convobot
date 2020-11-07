0f0221c3e0c8059a5c486a464194f25cc3d460e3
tupleu <tupleu@gmail.com>
2020-11-07 16:33:03 -0500

    add leave and kill commands

--- discord.js
+++ discord.js
@@ -1,33 +1,38 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
+
+const token = '';
+
 const client = new Discord.Client()
 
 client.once('ready', () => {
     console.log('Ready!');
 });
 
-client.login("Nzc0NzI1OTEyMzYwMjU1NTQw.X6b9uw.s8a-o8NabXQTNwhgx-5BIxkfmGM")
-
-client.on('message', message => {
-    console.log(message.content);
-    if (message.content === '!echo') {
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send('echo');
-    }
-});
+client.login(token)
 
 client.on('message', async message => {
-    // Voice only works in guilds, if the message does not come from a guild,
-    // we ignore it
-    if (!message.guild) return;
-  
-    if (message.content === '!join') {
-      // Only try to join the sender's voice channel if they are in one themselves
-      if (message.member.voice.channel) {
-        const connection = await message.member.voice.channel.join();
-      } else {
-        message.reply('You need to join a voice channel first!');
-      }
-      //This is going to be the stream that can go with Google stuff?
-      const ReadableStream = connection.receiver.createStream(message.member.user);
+    if(message.author == client.user) {
+        console.log('bot')
+        return;
     }
-  });
+    console.log(message.content);
+    if(message.content === '!echo') {
+        message.channel.send('echo');
+    }
+    else if(message.content === '!join') {
+        const voiceChannel = message.member.voice.channel
+        if(voiceChannel != null) {
+            const connection = await voiceChannel.join()
+        }
+        else {
+            message.reply('You need to join a voice channel first!');
+        }
+    }
+    else if(message.content === '!leave') {
+        console.log(message.guild.voice.channel)
+        message.guild.voice.channel.leave();
+    }
+    else if(message.content === '!kill') {
+        client.destroy()
+    }
+});
